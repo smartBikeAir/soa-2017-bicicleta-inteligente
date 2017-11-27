ef9a07aaf4d28f27a46def153e9686328c339eda
belenvergara <irma.b.vergara@gmail.com>
2017-11-26 23:40:24 -0300

    Mensajes Arduino
    Envio mensajes para arduino de configuracion.

--- Android/smart-bike-air/app/src/main/java/ar/so_unlam/edu/sba/FragmentConfiguracion.java
+++ Android/smart-bike-air/app/src/main/java/ar/so_unlam/edu/sba/FragmentConfiguracion.java
@@ -1,11 +1,16 @@
 package ar.so_unlam.edu.sba;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
 import android.util.Log;
+import android.widget.Toast;
+
+import static ar.so_unlam.edu.sba.AppConstants.RECIEVE_MESSAGE;
 
 /**
  * Created by irmab on 7/11/2017.
@@ -13,61 +18,57 @@ import android.util.Log;
 
 public class FragmentConfiguracion extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener{
 
-    public static final String KEY_PREF_DISTANCIA= "pref_unidad_distancia";
-    public static final String KEY_PREF_TIEMPO = "pref_unidad_tiempo";
     public static final String KEY_PREF_LUCES= "pref_luces";
-    public static final String KEY_PREF_ALARMA= "pref_alarma";
     public static final String KEY_PREF_PROXIMIDAD="pref_proximidad";
 
+    private static final AppService APP_SERVICE = AppServiceImpl.getInstance();
+
+    private ConnectedThread connectedThread = ((ConnectedThread)APP_SERVICE.getConnectedThread());
+
+    private Handler handler = new Handler();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.activity_preferences);
-
-      //Debemos preguntar antes de mostrar la actividad si tenemos conexion a bluetooth.
-
-        PreferenceManager preferenceManager = getPreferenceManager();
-        // Si  no tenemos coneccion
-        preferenceManager.findPreference(KEY_PREF_PROXIMIDAD).setEnabled(false);
-        // si tenemos coneccion
-
     }
 
 
     @Override
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
 
-        if (key.equals(KEY_PREF_DISTANCIA))
-        {
-            // Set summary to be the user-description for the selected value
-            Preference Pref = findPreference(key);
-                Pref.setSummary(sharedPreferences.getString(key, ""));
-                Log.i("", "Modifique Distancia: " + Pref.getKey());
-        }
-
-        if (key.equals(KEY_PREF_TIEMPO))
-        {
-
-
-        }
-
-        if (key.equals(KEY_PREF_ALARMA))
-        {
-
-
-        }
+        PreferenceManager preferenceManager = getPreferenceManager();
 
         if (key.equals(KEY_PREF_LUCES))
         {
+              if(preferenceManager.getSharedPreferences().getBoolean(KEY_PREF_LUCES,true)){
 
+                connectedThread.write(AppConstants.automaticLightDisabled+"\n");
+                preferenceManager.findPreference(KEY_PREF_LUCES).setDefaultValue(false);
 
-        }
+              }else {
+
+                  connectedThread.write(AppConstants.automaticLightEnabled+"\n");
+                  preferenceManager.findPreference(KEY_PREF_LUCES).setDefaultValue(true);
+
+              }
+          }
+
 
         if (key.equals(KEY_PREF_PROXIMIDAD))
         {
+            if(preferenceManager.getSharedPreferences().getBoolean(KEY_PREF_PROXIMIDAD,true)){
 
+                connectedThread.write(AppConstants.nearObjectDisabled+"\n");
+                preferenceManager.findPreference(KEY_PREF_PROXIMIDAD).setDefaultValue(false);
+
+            }else {
+
+                connectedThread.write(AppConstants.nearObjectEnabled+"\n");
+                preferenceManager.findPreference(KEY_PREF_PROXIMIDAD).setDefaultValue(true);
+
+            }
 
         }
     }
@@ -76,7 +77,7 @@ public class FragmentConfiguracion extends PreferenceFragment implements SharedP
     public void onResume() {
         super.onResume();
         getPreferenceManager().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
-
+        connectedThread.setHandler(handler);
 
     }
 
