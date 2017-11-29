8bf308b88696f0b629138c2042a99d5bc4d9c3e1
Alex Jerez <ajerez@despegar.com>
2017-11-29 17:08:13 -0300

    [Android] Refactors.

--- Android/smart-bike-air/app/src/main/java/ar/so_unlam/edu/sba/ConnectedThread.java
+++ Android/smart-bike-air/app/src/main/java/ar/so_unlam/edu/sba/ConnectedThread.java
@@ -3,7 +3,6 @@ package ar.so_unlam.edu.sba;
 import android.bluetooth.BluetoothSocket;
 import android.os.Handler;
 import android.util.Log;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
